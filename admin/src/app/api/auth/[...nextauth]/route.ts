93bfc0ca84ed5c1ce871bcdbb7857c7bf4fd9869
iamendor <andoruhrin@gmail.com>
2024-02-19 09:34:30 +0100

    login

--- admin/src/app/api/auth/[...nextauth]/route.ts
+++ admin/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
 import apolloClient from "@/apollo";
-import createApolloClient from "@/apollo";
 import { SIGN_IN } from "@/apollo/mutations";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
@@ -7,6 +6,17 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/signin",
     signOut: "/signout",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    jwt: ({ token, user }) => {
+      if (user) return { ...token, jwt: (user as any).jwt };
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (token) (session as any).jwt = token.jwt;
+      return session;
+    },
   },
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
